576671bf42462bc93ef8807dc0f50bcf442f418a
grahamcrowell <graham.crowell@gmail.com>
2016-08-13 03:30:02 -0700

    Stop Vms

---
+++ Azure/VirtualMachines/Stop Vms.ps1
@@ -0,0 +1,24 @@
+﻿$resourceGroupName = "vchds-root-rg"
+
+$Error.Clear()
+Get-AzureRmContext -ErrorAction Ignore
+$IsSignedIn=$true
+foreach ($eacherror in $Error) {
+    if ($eacherror.Exception.ToString() -like "*Run Login-AzureRmAccount to login.*") {
+        $IsSignedIn=$false
+    }
+}
+$Error.Clear()
+If($IsSignedIn -eq $false)
+{
+    Write-Host "signin to Azure"
+    Login-AzureRmAccount
+}
+
+$vms = Get-AzureRmVM -ResourceGroupName $resourceGroupName
+
+foreach($vm in $vms)
+{
+    Write-Host ("Stopping VM: {0}" -f $vm.Name)
+    Stop-AzureRmVM -Name $vm.Name -ResourceGroupName $resourceGroupName -Force
+}
