96af3c46950c70abcb4264ea2137285574cdb9b2
Crowell <GCrowell@vch.ca>
2017-04-04 11:55:53 -0700

    works!

--- json_deployment_templates/vch-iaas-deployment/deploy.ps1
+++ json_deployment_templates/vch-iaas-deployment/deploy.ps1
@@ -1,8 +1,16 @@
 Set-Location -Path $PSScriptRoot
 $template_path = Join-Path -Path $PSScriptRoot -ChildPath "template.json"
 $template_path = "template.json"
-Login-AzureRmAccount
+$parameter_path = "azuredeploy.parameters.json"
+# Login-AzureRmAccount
 # Set-AzureRmContext -SubscriptionID {your-subscription-ID}
-New-AzureRmResourceGroup -Name rg1 -Location "canadacentral"
-Test-AzureRmResourceGroupDeployment -ResourceGroupName rg1 -TemplateFile $template_path -Verbose -Debug
-New-AzureRmResourceGroupDeployment -Name rg1 -ResourceGroupName rg1 -TemplateFile $template_path
+
+$Secret = Read-Host -AsSecureString -Prompt "Enter VM local admin password. `nIt will save/OVERWRITE secret: VmAdminPassword `nin Key vault: vchDsKeyVault"
+# save pass word to Azure Key Vault
+Set-AzureKeyVaultSecret -VaultName 'vchDsKeyVault' -Name 'VmAdminPassword' -SecretValue $Secret
+# enable Key Vault for template deployment
+Set-AzureRmKeyVaultAccessPolicy -VaultName 'vchDsKeyVault' -EnabledForTemplateDeployment
+
+New-AzureRmResourceGroup -Name rg2 -Location "canadacentral"
+Test-AzureRmResourceGroupDeployment -ResourceGroupName rg2 -TemplateFile $template_path -TemplateParameterFile $parameter_path -Verbose -Debug
+New-AzureRmResourceGroupDeployment -Name gcdeployment -ResourceGroupName rg2 -TemplateFile $template_path -TemplateParameterFile $parameter_path
