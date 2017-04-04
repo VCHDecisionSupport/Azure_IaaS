7b245e68b329b201877695a812465a363f6b7b33
Crowell <GCrowell@vch.ca>
2017-04-03 18:50:17 -0700

    nearly working

--- json_deployment_templates/vch-iaas-deployment/deploy.ps1
+++ json_deployment_templates/vch-iaas-deployment/deploy.ps1
@@ -3,6 +3,7 @@ $template_path = Join-Path -Path $PSScriptRoot -ChildPath "template.json"
 $template_path = "template.json"
 # Login-AzureRmAccount
 # Set-AzureRmContext -SubscriptionID {your-subscription-ID}
-# New-AzureRmResourceGroup -Name DevOpRg -Location "canadacentral"
-Test-AzureRmResourceGroupDeployment -ResourceGroupName DevOpRg -TemplateFile $template_path
-# New-AzureRmResourceGroupDeployment -Name ExampleDeployment -ResourceGroupName DevOpRg -TemplateFile $template_path
+New-AzureRmResourceGroup -Name Bob5 -Location "canadacentral"
+Test-AzureRmResourceGroupDeployment -ResourceGroupName Bob5 -TemplateFile $template_path -Verbose -Debug
+New-AzureRmResourceGroupDeployment -Name Bob5 -ResourceGroupName Bob5 -TemplateFile $template_path
+canadacentral
