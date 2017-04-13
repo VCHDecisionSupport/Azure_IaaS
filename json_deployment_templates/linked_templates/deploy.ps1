dab5cdcba8bf47551dba7b5908d028e90b118911
gcrowell vch ds <graham.crowell@gmail.com>
2017-04-13 16:42:39 -0700

    change parameter to variable

--- json_deployment_templates/linked_templates/deploy.ps1
+++ json_deployment_templates/linked_templates/deploy.ps1
@@ -2,7 +2,7 @@ Set-Location -Path $PSScriptRoot
 $template_path = Join-Path -Path $PSScriptRoot -ChildPath "template.json"
 $template_path = "azuredeploy.json"
 # $parameter_path = "azuredeploy.parameters.json"
-$resource_group_name = "testvchrg"
+$resource_group_name = "testvchrg0"
 
 # Login-AzureRmAccount
 
@@ -11,4 +11,4 @@ New-AzureRmResourceGroup -Name $resource_group_name -Location "canadacentral"
 Test-AzureRmResourceGroupDeployment -ResourceGroupName $resource_group_name -TemplateFile $template_path
 #  -TemplateParameterFile $parameter_path -Verbose -Debug
 
-# New-AzureRmResourceGroupDeployment -Name gcdeployment -ResourceGroupName $resource_group_name -TemplateFile $template_path
+New-AzureRmResourceGroupDeployment -Name gcdeployment -ResourceGroupName $resource_group_name -TemplateFile $template_path
