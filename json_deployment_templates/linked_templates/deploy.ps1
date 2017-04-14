49c9a1aab01472d9b0228f217d2bdf516efdecf5
gcrowell vch ds <graham.crowell@gmail.com>
2017-04-13 17:05:14 -0700

    rename deployment resources

--- json_deployment_templates/linked_templates/deploy.ps1
+++ json_deployment_templates/linked_templates/deploy.ps1
@@ -1,14 +1,14 @@
 Set-Location -Path $PSScriptRoot
-$template_path = Join-Path -Path $PSScriptRoot -ChildPath "template.json"
+$template_path = Join-Path -Path $PSScriptRoot -ChildPath "azuredeploy.json"
 $template_path = "azuredeploy.json"
 # $parameter_path = "azuredeploy.parameters.json"
-$resource_group_name = "testvchrg0"
+$resource_group_name = "testvchrg"
 
 # Login-AzureRmAccount
 
-New-AzureRmResourceGroup -Name $resource_group_name -Location "canadacentral"
+# New-AzureRmResourceGroup -Name $resource_group_name -Location "canadacentral"
 
 Test-AzureRmResourceGroupDeployment -ResourceGroupName $resource_group_name -TemplateFile $template_path
 #  -TemplateParameterFile $parameter_path -Verbose -Debug
 
-# New-AzureRmResourceGroupDeployment -Name gcdeployment -ResourceGroupName $resource_group_name -TemplateFile $template_path
+New-AzureRmResourceGroupDeployment -Name gcdeployment -ResourceGroupName $resource_group_name -TemplateFile $template_path
