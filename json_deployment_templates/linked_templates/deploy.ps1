968da20a50e188dd6cca2a410edaa4edb5472a0c
gcrowell vch ds <graham.crowell@gmail.com>
2017-04-13 17:29:12 -0700

    hard code workloadConfig

--- json_deployment_templates/linked_templates/deploy.ps1
+++ json_deployment_templates/linked_templates/deploy.ps1
@@ -2,12 +2,12 @@ Set-Location -Path $PSScriptRoot
 $template_path = Join-Path -Path $PSScriptRoot -ChildPath "azuredeploy.json"
 $template_path = "azuredeploy.json"
 # $parameter_path = "azuredeploy.parameters.json"
-$resource_group_name = "testvchrg2"
+$resource_group_name = "testvchrg3"
 
 # Login-AzureRmAccount
 
-# New-AzureRmResourceGroup -Name $resource_group_name -Location "canadacentral"
+New-AzureRmResourceGroup -Name $resource_group_name -Location "canadacentral"
 
 Test-AzureRmResourceGroupDeployment -ResourceGroupName $resource_group_name -TemplateFile $template_path
 
-# New-AzureRmResourceGroupDeployment -Name gcdeployment -ResourceGroupName $resource_group_name -TemplateFile $template_path
+New-AzureRmResourceGroupDeployment -Name gcdeployment -ResourceGroupName $resource_group_name -TemplateFile $template_path
