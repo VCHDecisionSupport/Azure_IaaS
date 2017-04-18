b43ba9a3cea8d3497ce57abf04225fc91359eacb
gcrowell vch ds <graham.crowell@gmail.com>
2017-04-18 12:23:19 -0700

    add subnet template

--- json_deployment_templates/linked_templates/deploy.ps1
+++ json_deployment_templates/linked_templates/deploy.ps1
@@ -3,7 +3,7 @@ Set-Location -Path $PSScriptRoot
 $template_path = "azuredeploy.json"
 # $template_path = "azuredeploy_subnets.json"
 # $parameter_path = "azuredeploy.parameters.json"
-$resource_group_name = "testrg"
+$resource_group_name = "testrg1"
 
 # prompts login popup; only need to login once per powershell session
 # Login-AzureRmAccount
@@ -12,4 +12,4 @@ New-AzureRmResourceGroup -Name $resource_group_name -Location "canadacentral"
 
 Test-AzureRmResourceGroupDeployment -ResourceGroupName $resource_group_name -TemplateFile $template_path
 
-New-AzureRmResourceGroupDeployment -Name gcdeployment -ResourceGroupName $resource_group_name -TemplateFile $template_path
+New-AzureRmResourceGroupDeployment -Name mainEntryDeployment -ResourceGroupName $resource_group_name -TemplateFile $template_path
