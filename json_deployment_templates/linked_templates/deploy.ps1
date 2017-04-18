440e69dd87165d47652ac849da98fb5b851fe8bc
gcrowell vch ds <graham.crowell@gmail.com>
2017-04-18 13:32:45 -0700

    remove copyIndex() parameter

--- json_deployment_templates/linked_templates/deploy.ps1
+++ json_deployment_templates/linked_templates/deploy.ps1
@@ -6,7 +6,7 @@ $template_path = "azuredeploy.json"
 $resource_group_name = "testrg0"
 
 # prompts login popup; only need to login once per powershell session
-# Login-AzureRmAccount
+Login-AzureRmAccount
 
 New-AzureRmResourceGroup -Name $resource_group_name -Location "canadacentral"
 
