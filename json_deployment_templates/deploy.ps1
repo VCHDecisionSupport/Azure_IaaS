7571d1ea41885cbbd4d637bd2859d1e1ae3ccca6
gcrowell vch ds <graham.crowell@gmail.com>
2017-04-20 16:47:56 -0700

    vms depends on storage

--- json_deployment_templates/deploy.ps1
+++ json_deployment_templates/deploy.ps1
@@ -1,9 +1,11 @@
+# https://github.com/VCHDecisionSupport/azure-quickstart-templates/tree/master/sharepoint-three-vm
+
 # set current working directory
 Set-Location -Path $PSScriptRoot
 $template_path = "azuredeploy.json"
 # $template_path = "azuredeploy_subnets.json"
 # $parameter_path = "azuredeploy.parameters.json"
-$resource_group_name = "testrg"
+$resource_group_name = "testrg0"
 
 # prompts login popup; only need to login once per powershell session
 # Login-AzureRmAccount
