e7b8c2e214db655891aeef3b02c90a7370229b31
Graham Crowell <graham.crowell@gmail.com>
2017-04-21 13:57:15 -0700

    please work

--- json_deployment_templates/deploy.ps1
+++ json_deployment_templates/deploy.ps1
@@ -1,13 +1,13 @@
 # https://github.com/VCHDecisionSupport/azure-quickstart-templates/tree/master/sharepoint-three-vm
-
+# https://github.com/Azure/azure-quickstart-templates/blob/master/201-vm-dynamic-data-disks-selection/azuredeploy.json
 # set current working directory
 Set-Location -Path $PSScriptRoot
-$resource_group_name = "testrg2"
+$resource_group_name = "testrg3"
 
 # prompts login popup; only need to login once per powershell session
-Login-AzureRmAccount
+# Login-AzureRmAccount
 
-New-AzureRmResourceGroup -Name $resource_group_name -Location "canadacentral"
+# New-AzureRmResourceGroup -Name $resource_group_name -Location "canadacentral"
 
 
 $template_path = "azuredeploy_shared.json"
