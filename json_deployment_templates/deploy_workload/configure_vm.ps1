680560af98088c51cee4c28a3539e351b70a5410
gcrowell vch ds <graham.crowell@gmail.com>
2017-04-24 16:44:19 -0700

    delete custom script attempts

--- json_deployment_templates/deploy_workload/configure_vm.ps1
+++
@@ -1,1 +0,0 @@
-New-Item -Name "thisisatest.txt"
--- json_deployment_templates/deploy_workload/deploy_script.ps1
+++
@@ -1,5 +0,0 @@
-Set-Location -Path $PSScriptRoot
-$resource_group_name = "testrg"
-$template_path = "script_extension.json"
-
-New-AzureRmResourceGroupDeployment -Name sharedResourcesDeployment1 -ResourceGroupName $resource_group_name -TemplateFile $template_path
