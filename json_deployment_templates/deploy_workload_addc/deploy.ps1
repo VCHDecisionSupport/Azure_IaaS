d6840de388df87f16bfc1fe7b92b8fc16554e2f0
gcrowell vch ds <graham.crowell@gmail.com>
2017-04-24 13:38:28 -0700

    remove storage and vnet from ADDC deployment

---
+++ json_deployment_templates/deploy_workload_addc/deploy.ps1
@@ -0,0 +1,13 @@
+Set-Location -Path $PSScriptRoot
+$resource_group_name = "addcquickstart"
+$template_path = "azuredeploy.json"
+$parameter_path = "azuredeploy.parameters.json"
+$location = "canadacentral"
+$deployment_name = "addcDeployment"
+
+
+# Login-AzureRmAccount
+
+# New-AzureRmResourceGroup -Name $resource_group_name -Location $location
+
+New-AzureRmResourceGroupDeployment -Name $deployment_name -ResourceGroupName $resource_group_name -TemplateFile $template_path -TemplateParameterFile $parameter_path
