4c690d57d09c6d026f074be1f991a00244f670a5
gcrowell vch ds <graham.crowell@gmail.com>
2017-04-27 13:16:44 -0700

    install azure

--- powershell_utils/Install-Azure.ps1
+++ powershell_utils/Install-Azure.ps1
@@ -1,5 +1,4 @@
-
-# https://docs.microsoft.com/en-us/powershell/azure/install-azurerm-ps?view=azurermps-3.8.0
+# based on: https://docs.microsoft.com/en-us/powershell/azure/install-azurerm-ps?view=azurermps-3.8.0
 Get-Module PowerShellGet -list | Select-Object Name,Version,Path
 
 Install-Module AzureRM
@@ -11,3 +10,14 @@ Write-Host "`n`n`tLogin using popup! press ALT+TAB`n`n"
 Login-AzureRmAccount
 
 New-AzureRmResourceGroup -Name $env:USERNAME -Location $location
+
+$sub_info=Get-AzureSubscription
+$sub_info_string=("SubscriptionName: {0}  SubscriptionId: {1}" -f $sub_info.SubscriptionName, $sub_info.SubscriptionId)
+
+Write-Host "`n`n-------------Subscription Info Start---------------"
+Write-Host $sub_info_string
+Write-Host "-------------Subscription Info End---------------"
+
+Write-Host "`n`nEmail Subscription Info Graham..."
+
+Start-Process "mailto:graham.crowell@vch.ca?Subject=Azure login info for $env:USERNAME&Body=$sub_info_string"
