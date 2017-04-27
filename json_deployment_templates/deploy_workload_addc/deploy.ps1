ebeb2630aa552318bb33fb1fcef39c170be98114
gcrowell vch ds <graham.crowell@gmail.com>
2017-04-27 11:46:27 -0700

    change storage account to be a parameter

--- json_deployment_templates/deploy_workload_addc/deploy.ps1
+++ json_deployment_templates/deploy_workload_addc/deploy.ps1
@@ -1,13 +1,13 @@
 Set-Location -Path $PSScriptRoot
-$resource_group_name = "addcvchds"
+$resource_group_name = "addcvchdsrg"
 $template_path = "azuredeploy.json"
 $parameter_path = "azuredeploy.parameters.json"
 $location = "canadacentral"
 $deployment_name = "addcDeployment"
 
 
-Login-AzureRmAccount
+# Login-AzureRmAccount
 
-New-AzureRmResourceGroup -Name $resource_group_name -Location $location
+# New-AzureRmResourceGroup -Name $resource_group_name -Location $location
 
 New-AzureRmResourceGroupDeployment -Name $deployment_name -ResourceGroupName $resource_group_name -TemplateFile $template_path -TemplateParameterFile $parameter_path
