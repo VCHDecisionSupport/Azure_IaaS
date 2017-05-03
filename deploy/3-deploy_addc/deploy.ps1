3a3bfa76ed5118ddd70237459ee6ab847f7a6cf9
Graham Crowell <graham.crowell@gmail.com>
2017-05-03 14:05:32 -0700

    add auto update to deploy.ps1 for addc

--- deploy/3-deploy_addc/deploy.ps1
+++ deploy/3-deploy_addc/deploy.ps1
@@ -9,6 +9,20 @@ $resource_group_name = "vchds-sp-test-rg"
 # Azure login; only need to login once per powershell session
 # Login-AzureRmAccount
 
+# update parameter file with Automation Account info
+Write-Host "`n`nattempting to automatically update parameter file: dscRegistrationKey and dscRegistrationUrl"
+Write-Host "connecting to automation account"
+$automation_account_name = "vchds-auto"
+$automation_account_info = Get-AzureRmAutomationRegistrationInfo -ResourceGroupName $resource_group_name -AutomationAccountName 
+$pathToJson = "azuredeploy.parameters.json"
+Write-Host "editting json file: ${pathToJson} in ${PSScriptRoot}"
+$json = Get-Content $pathToJson | ConvertFrom-Json
+$json.parameters.dscRegistrationKey.value = $automation_account_info.PrimaryKey
+$json.parameters.dscRegistrationUrl.value = $automation_account_info.Endpoint
+$json | ConvertTo-Json | set-content $pathToJson
+Write-Host "`nparameter file updated`n"
+
+
 # deploy resources declared in $template_path
 $template_path = "azuredeploy.json"
 $parameter_path = "azuredeploy.parameters.json"
