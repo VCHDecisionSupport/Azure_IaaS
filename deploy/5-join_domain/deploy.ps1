0e56606cc4f3415c5806e7049a99bca6bd9c7318
grahamcrowell_vch <graham.crowell@gmail.com>
2017-05-03 11:32:03 -0700

    step 5 join domain

---
+++ deploy/5-join_domain/deploy.ps1
@@ -0,0 +1,16 @@
+# set current working directory
+Set-Location -Path $PSScriptRoot
+$resource_group_name = "vchds-sp-test-rg"
+$location = "canadacentral"
+
+# Azure login; only need to login once per powershell session
+# Login-AzureRmAccount
+
+# deploy resources declared in $template_path
+$template_path = "azuredeploy.json"
+$parameter_path = "azuredeploy.parameters.json"
+$deployment_name = "joinDomainDeployment"
+Write-Host ("Testing deployment of template:`n`t{0}" -f $template_path)
+Test-AzureRmResourceGroupDeployment -ResourceGroupName $resource_group_name -TemplateFile $template_path -TemplateParameterFile $parameter_path
+Write-Host ("Deploying of template:`n`t{0}" -f $template_path)
+New-AzureRmResourceGroupDeployment -Name $deployment_name -ResourceGroupName $resource_group_name -TemplateFile $template_path -TemplateParameterFile $parameter_path
