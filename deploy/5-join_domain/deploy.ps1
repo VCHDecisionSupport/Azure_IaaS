29639ac9e6b26fae53a02e55af3bb2adcc155cc5
gcrowell vch ds <graham.crowell@gmail.com>
2017-05-04 17:04:41 -0700

    updated vm names

--- deploy/5-join_domain/deploy.ps1
+++ deploy/5-join_domain/deploy.ps1
@@ -4,7 +4,7 @@ $resource_group_name = "vchds-sp-test-rg"
 $location = "canadacentral"
 
 # Azure login; only need to login once per powershell session
-# Login-AzureRmAccount
+Login-AzureRmAccount
 
 # deploy resources declared in $template_path
 $template_path = "azuredeploy.json"
