de70c81aef660bb8a2236b674960c16a1acbcd35
gcrowell vch ds <graham.crowell@gmail.com>
2017-05-08 20:01:38 -0700

    add key vault to deployment

--- deploy/1-deploy_network/deploy.ps1
+++ deploy/1-deploy_network/deploy.ps1
@@ -2,31 +2,43 @@
 # set current working directory
 Set-Location -Path $PSScriptRoot
 
-$resource_group_name = "vchds-sp-test-rg"
-
+$resource_group_name = "vchds-sp-test-rg2"
+$tenant_id = (Get-AzureRMSubscription | Where-Object { $_.State -eq "Enabled"})[0].TenantId
 
 # Azure login; only need to login once per powershell session
-Add-AzureRmAccount
+# Add-AzureRmAccount
 
 $location = "canadacentral"
-New-AzureRmResourceGroup -Name $resource_group_name -Location $location
+# New-AzureRmResourceGroup -Name $resource_group_name -Location $location
+
 
 # deploy resources declared in $template_path
-$template_path = "azuredeploy_vnet.json"
-$parameter_path = "azuredeploy_vnet.parameters.json"
-$deployment_name = "vnetDeployment"
+$template_path = "azuredeploy_keyVault.json"
+$parameter_path = "azuredeploy_keyVault.parameters.json"
+$deployment_name = "keyVaultDeployment"
 Write-Host ("Testing deployment of template:`n`t{0}" -f $template_path)
 Test-AzureRmResourceGroupDeployment -ResourceGroupName $resource_group_name -TemplateFile $template_path -TemplateParameterFile $parameter_path -Verbose
 Write-Host ("Deploying of template:`n`t{0}" -f $template_path)
 New-AzureRmResourceGroupDeployment -Name $deployment_name -ResourceGroupName $resource_group_name -TemplateFile $template_path -TemplateParameterFile $parameter_path -Verbose
 
-# deploy resources declared in $template_path
-$template_path = "azuredeploy_storage.json"
-$parameter_path = "azuredeploy_storage.parameters.json"
-$deployment_name = "storageDeployment"
-Write-Host ("Testing deployment of template:`n`t{0}" -f $template_path)
-Test-AzureRmResourceGroupDeployment -ResourceGroupName $resource_group_name -TemplateFile $template_path -TemplateParameterFile $parameter_path -Verbose
-Write-Host ("Deploying of template:`n`t{0}" -f $template_path)
-New-AzureRmResourceGroupDeployment -Name $deployment_name -ResourceGroupName $resource_group_name -TemplateFile $template_path -TemplateParameterFile $parameter_path -Verbose
+
+
+# # deploy resources declared in $template_path
+# $template_path = "azuredeploy_vnet.json"
+# $parameter_path = "azuredeploy_vnet.parameters.json"
+# $deployment_name = "vnetDeployment"
+# Write-Host ("Testing deployment of template:`n`t{0}" -f $template_path)
+# Test-AzureRmResourceGroupDeployment -ResourceGroupName $resource_group_name -TemplateFile $template_path -TemplateParameterFile $parameter_path -Verbose
+# Write-Host ("Deploying of template:`n`t{0}" -f $template_path)
+# New-AzureRmResourceGroupDeployment -Name $deployment_name -ResourceGroupName $resource_group_name -TemplateFile $template_path -TemplateParameterFile $parameter_path -Verbose
+
+# # deploy resources declared in $template_path
+# $template_path = "azuredeploy_storage.json"
+# $parameter_path = "azuredeploy_storage.parameters.json"
+# $deployment_name = "storageDeployment"
+# Write-Host ("Testing deployment of template:`n`t{0}" -f $template_path)
+# Test-AzureRmResourceGroupDeployment -ResourceGroupName $resource_group_name -TemplateFile $template_path -TemplateParameterFile $parameter_path -Verbose
+# Write-Host ("Deploying of template:`n`t{0}" -f $template_path)
+# New-AzureRmResourceGroupDeployment -Name $deployment_name -ResourceGroupName $resource_group_name -TemplateFile $template_path -TemplateParameterFile $parameter_path -Verbose
 
 
