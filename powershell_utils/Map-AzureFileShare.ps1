f6d0e803936bf807c0f3c25cab7f75f995379be0
grahamcrowell_vch <graham.crowell@gmail.com>
2017-05-09 13:25:42 -0700

    update drive letter

--- powershell_utils/Map-AzureFileShare.ps1
+++ powershell_utils/Map-AzureFileShare.ps1
@@ -4,4 +4,4 @@ $storage_account_name = "vchdsgeneralstorage"
 $storage_account_pass = "ZYLpWwtoR9Pn7gftHom21gJXDjhFtnrJqQJsDLMBiv2fOsfefrg86En0T5PkC7RInsqsPDyXKr4l/N542nhYJQ=="
 
 cmdkey /add:vchdsgeneralstorage.file.core.windows.net /user:AZURE\vchdsgeneralstorage /pass:ZYLpWwtoR9Pn7gftHom21gJXDjhFtnrJqQJsDLMBiv2fOsfefrg86En0T5PkC7RInsqsPDyXKr4l/N542nhYJQ==
-net use A: \\vchdsgeneralstorage.file.core.windows.net\vchdsfileshare
+net use S: \\vchdsgeneralstorage.file.core.windows.net\vchdsfileshare
