84c517b50c9e011d83b4f74a2d60d1c756ac0bf0
Graham Crowell Azure devVm0 <graham.crowell@gmail.com>
2017-05-09 19:47:10 +0000

    check drive not working

--- powershell_utils/Check-FileShare.ps1
+++ powershell_utils/Check-FileShare.ps1
@@ -6,13 +6,18 @@ $cmd = {
     $ps_drive = Get-PSDrive -PSProvider "FileSystem" | Where-Object {$_.DisplayRoot -eq $share_display_root}
     Write-Host $ps_drive
     if ($ps_drive -eq $null) {
-        Write-Host "MOUNTING $share_display_root to ${ps_drive}:"
-        cmdkey /add:vchdsgeneralstorage.file.core.windows.net /user:AZURE\vchdsgeneralstorage /pass:ZYLpWwtoR9Pn7gftHom21gJXDjhFtnrJqQJsDLMBiv2fOsfefrg86En0T5PkC7RInsqsPDyXKr4l/N542nhYJQ==
-        net use $drive_letter \\vchdsgeneralstorage.file.core.windows.net\vchdsfileshare
+        Write-Host "$share_display_root is NOT MOUNTED ${drive_letter}:"
+        # cmdkey /add:vchdsgeneralstorage.file.core.windows.net /user:AZURE\vchdsgeneralstorage /pass:ZYLpWwtoR9Pn7gftHom21gJXDjhFtnrJqQJsDLMBiv2fOsfefrg86En0T5PkC7RInsqsPDyXKr4l/N542nhYJQ==
+        # net use $drive_letter \\vchdsgeneralstorage.file.core.windows.net\vchdsfileshare
     }
     else {
-        Write-Host "$share_display_root is ALREADY mounted to ${ps_drive}:"
+        Write-Host "$share_display_root is ALREADY mounted to ${drive_letter}:"
     }
 }
 
-Invoke-Command -ScriptBlock $cmd
+$pass = ConvertTo-SecureString "Floater1" -AsPlainText -Force
+$user = "spVm2"
+$cred = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $user, $pass
+
+Invoke-Command -ComputerName "spVm2" -ScriptBlock $cmd -Credential $cred
+Invoke-Command -ComputerName "spVm2" -ScriptBlock {Get-PSDrive -PSProvider "FileSystem" | Where-Object {$_.DisplayRoot -eq $share_display_root}} -Credential $cred
