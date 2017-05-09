4582334ebc90b35976920e209eda5b30c68db7b4
Graham Crowell Azure devVm0 <graham.crowell@gmail.com>
2017-05-09 21:46:39 +0000

    consodate dirv econfig

--- powershell_utils/Init-Drives.ps1
+++ powershell_utils/Init-Drives.ps1
@@ -1,9 +1,40 @@
+#########################################################################################
+##########        Mount Azure File Share as S: Drive
+#########################################################################################
+$cmd = {
+    $drive_letter = "S:"
+    $share_display_root = "\\vchdsgeneralstorage.file.core.windows.net\vchdsfileshare"
+    $ps_drive = Get-PSDrive -PSProvider "FileSystem" | Where-Object {$_.DisplayRoot -eq $share_display_root}
+    Write-Host $ps_drive
+    if ($ps_drive -eq $null) {
+        Write-Host "MOUNTING $share_display_root to ${ps_drive}:"
+        cmdkey /add:vchdsgeneralstorage.file.core.windows.net /user:AZURE\vchdsgeneralstorage /pass:ZYLpWwtoR9Pn7gftHom21gJXDjhFtnrJqQJsDLMBiv2fOsfefrg86En0T5PkC7RInsqsPDyXKr4l/N542nhYJQ==
+        net use $drive_letter \\vchdsgeneralstorage.file.core.windows.net\vchdsfileshare
+    }
+    else 
+    {
+        Write-Host "$share_display_root is ALREADY mounted to ${ps_drive}:"
+    }
+}
+
+Invoke-Command -ScriptBlock $cmd
+
+
+
+#########################################################################################
+##########        Initialize, Format Azure Data Disks
+#########################################################################################
+
+
 # https://docs.microsoft.com/en-us/azure/virtual-machines/windows/attach-disk-ps#add-an-empty-data-disk-to-a-virtual-machine
 
 
 # 1. see uninit disks
 $disks = Get-Disk | Where partitionstyle -Like 'raw' | sort number
 $disks
+Get-PSDrive -PSProvider "FileSystem"
+
+
 
 # setup up config dict: {lun,{letter, label}}
 # for spVm0, spVm1, 
@@ -14,6 +45,8 @@ $lun2drive = @{
 }
 
 $lun2drive
+
+Read-Host "confirm..."
 
 foreach ($lun in $lun2drive.Keys) {
     Write-Host "mapping ${lun2drive[$lun]}"
