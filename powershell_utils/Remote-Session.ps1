f31866cd57b86475f851cdaf1eba9f5a2df3f2c2
Graham Crowell Azure devVm0 <graham.crowell@gmail.com>
2017-05-09 19:37:45 +0000

    fileshare

--- powershell_utils/Remote-Session.ps1
+++ powershell_utils/Remote-Session.ps1
@@ -1,8 +1,8 @@
 # https://www.howtogeek.com/117192/how-to-run-powershell-commands-on-remote-computers/
 
 $pass = ConvertTo-SecureString "Floater1" -AsPlainText -Force
-$user = "vch\spVm0"
+$user = "spVm2"
 $cred = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $user, $pass
-$target = "spVm0"
+$target = "spVm2"
 Enter-PSSession -ComputerName $target -Credential $cred
 
