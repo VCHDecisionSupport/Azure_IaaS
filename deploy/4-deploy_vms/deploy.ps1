0b22818f29fd37691b3b5238d221a210c0f2fbc5
gcrowell vch ds <graham.crowell@gmail.com>
2017-05-08 18:11:55 -0700

    fix deploy.ps1 $templatepath

--- deploy/4-deploy_vms/deploy.ps1
+++ deploy/4-deploy_vms/deploy.ps1
@@ -4,10 +4,11 @@ $resource_group_name = "vchds-sp-test-rg"
 $location = "canadacentral"
 
 # Azure login; only need to login once per powershell session
-# Add-AzureRmAccount
+Add-AzureRmAccount
 
 # deploy resources declared in $template_path
 $parameter_path = "azuredeploy.parameters.json"
+$template_path = "azuredeploy.json"
 $deployment_name = "vmDeployment"
 Write-Host ("Testing deployment of template:`n`t{0}" -f $template_path)
 Test-AzureRmResourceGroupDeployment -ResourceGroupName $resource_group_name -TemplateFile $template_path -TemplateParameterFile $parameter_path -Verbose
