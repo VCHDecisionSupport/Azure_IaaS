c5a6973b48081e560300a57dcd4680f110782c7c
Graham Crowell Azure devVm0 <graham.crowell@gmail.com>
2017-05-10 02:20:03 +0000

    asdf

--- powershell_utils/Add-ADUsers.ps1
+++ powershell_utils/Add-ADUsers.ps1
@@ -1,4 +1,4 @@
-$pass = ConvertTo-SecureString "Floater1" -AsPlainText -Force
+$pass = ConvertTo-SecureString "#DrainTheSwamp" -AsPlainText -Force
 $user = "vch\dcVm0"
 $cred = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $user, $pass
 $comp = "dcVm0"
@@ -7,12 +7,13 @@ Write-Host "--------------------------------------------`n$comp`n---------------
 
 
 $cmd = {
-
-    New-ADUser -
-
+    New-ADUser -Name overlord -AccountExpirationDate $null -ChangePasswordAtLogon $false -SamAccountName overlord -AccountPassword $pass -Enabled $true
 }
-Invoke-Command -ComputerName $comp -ScriptBlock $cmd -Credential $cred
+# Invoke-Command -ComputerName $comp -ScriptBlock $cmd -Credential $cred
 
 
 
 Write-Host "--------------------------------------------`n$comp`n--------------------------------------------`n"
+
+
+Get-ADUser -Filter "Name -like 'overlord'" -Properties *
