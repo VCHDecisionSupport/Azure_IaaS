678518e6b995ab7f7e1790c6edaef701b713b59a
azure dev jumpbox <graham.crowell.vch@outlook.com>
2017-05-10 23:24:15 +0000

    new user validated by dynamic owl

---
+++ powershell_utils/New-User.ps1
@@ -0,0 +1,30 @@
+﻿# Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force -Confirm:$false
+# Import-Module ActiveDirectory
+Set-Location -Path $PSScriptRoot
+$pass = ConvertTo-SecureString "#DrainTheSwamp" -AsPlainText -Force
+$user = "SVC_SPDBSSPS008_SQL"
+
+$aduser = Get-ADUser -Filter "Name -like '$user'" -Properties *
+if($aduser -eq $null)
+{
+    New-ADUser -Name $user `
+        -AccountExpirationDate $null `
+        -AccountNotDelegated $false `
+        -AccountPassword $pass `
+        -CannotChangePassword $true `
+        -ChangePasswordAtLogon $false `
+        -DisplayName $user `
+        -Enabled $true `
+        -SamAccountName $user `
+        -TrustedForDelegation $false # if kyperors then this is true on webapp account
+    
+    $aduser = Get-ADUser -Filter "Name -like '$user'" -Properties *
+    .\Set-ADAccountasLocalAdministrator.ps1 -Computer 'spVm3' -Trustee "vch\$user"
+    
+}
+
+$aduser
+#.\Set-ADAccountasLocalAdministrator.ps1 -Computer 'spVm0,spVm1,spVm2,spVm3,dcVm0,devVm0' -Trustee vch\overlord
+# New-ADGroup -Name service -DisplayName service -SamAccountName service
+Get-ADGroup -Filter "CanonicalName -like '*HealthBC.org/Health/HSSBC/User Accounts/Service*'"
+Get-ADUser -Filter "Name -like 'overlord'" -Properties *
