46b39ddbd090b621cc104999c6e5b6be3fe48fb0
azure dev jumpbox <graham.crowell.vch@outlook.com>
2017-05-12 19:36:46 +0000

    delte unifned sp

--- powershell_utils/Install-Sp/RemoteInstall.ps1
+++
@@ -1,5 +0,0 @@
-$pass = ConvertTo-SecureString "#DrainTheSwamp" -AsPlainText -Force
-$user = "svc_ds_sp_farm"
-$cred = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $user, $pass
-$target = "spVm1"
-Enter-PSSession -ComputerName $target -Credential $cred
